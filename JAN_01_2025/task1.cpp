7f9460d3a9b2fc353b0679b99ec6f8b1cc9dca6a
Palak-gupta18 <palakgupta182002@gmail.com>
2025-01-01 20:01:14 +0530

    Create task1.cpp

---
+++ JAN_01_2025/task1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+    bool isThree(int n) {
+        int count =0;
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                count++;
+            }
+            if(count>3){
+               return false;
+            }
+        }
+        if(count==3){
+        return true;
+        }
+        return false;
+    }
+    int main(){
+        int n = 2;
+        if(isThree(n)){
+            cout<<"True"<<endl;
+        }
+        else{
+            cout<<"False"<<endl;
+        }
+    }
